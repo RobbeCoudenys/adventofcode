992023771a87b7b3b79c379837d329e0e00032ef
Robbe Coudenys <robbe.coudenys@gmail.com>
2024-12-07 21:44:37 +0100

    day7 part1

--- src/_2024/day6/solution.rs
+++ src/_2024/day6/solution.rs
@@ -16,6 +16,7 @@ enum Direction {
 }
 
 impl Direction {
+    #[inline]
     fn get_next_coords(&self, coords: Coords) -> Coords {
         match self {
             Direction::North => (coords.0, coords.1 - 1),
@@ -25,6 +26,7 @@ impl Direction {
         }
     }
 
+    #[inline]
     fn rotate(&self) -> Direction {
         match self {
             Direction::North => Direction::East,
@@ -47,15 +49,18 @@ struct Guard {
 impl Guard {
     // Walks the guard in a repetitive pattern until it goes outside the map
     // or enters an infinite loop
-    fn walk(&mut self, map: &HashMap<Coords, char>) -> bool {
+    fn walk(&mut self, map: &HashMap<Coords, char>, obstacle: Option<&Coords>) -> bool {
         let next_coords = self.direction.get_next_coords(self.coords);
         if let Some(next_char) = map.get(&next_coords) {
-            if next_char == &'#' {
+            if next_char == &'#' || Some(&next_coords) == obstacle {
                 self.direction = self.direction.rotate();
-                return self.walk(map);
+                return self.walk(map, obstacle);
             }
         }
         self.coords = next_coords;
+        if Option::None == map.get(&next_coords) {
+            return false;
+        }
         if let Some(prev_directions) = self.previous_positions.get_mut(&self.coords) {
             if prev_directions.contains(&self.direction) {
                 return true;
@@ -82,6 +87,8 @@ fn parse_input(input: String) -> HashMap<Coords, char> {
 fn find_guard(map: &HashMap<Coords, char>) -> Guard {
     for (coords, c) in map {
         if *c == 'v' || *c == '^' || *c == '<' || *c == '>' {
+            let mut previous_positions = HashMap::new();
+            previous_positions.insert(*coords, [Direction::North].iter().cloned().collect());
             return Guard {
                 coords: *coords,
                 direction: match c {
@@ -91,43 +98,39 @@ fn find_guard(map: &HashMap<Coords, char>) -> Guard {
                     'v' => Direction::South,
                     _ => panic!("Invalid direction for char: {}", c),
                 },
-                previous_positions: HashMap::new(),
+                previous_positions,
             };
         }
     }
     panic!("No guard found in map");
 }
 
-fn traverse_map(mut map: HashMap<Coords, char>, mut guard: Guard) -> (HashMap<Coords, char>, bool) {
-    if let Some(curr_pos) = map.get_mut(&guard.coords) {
-        *curr_pos = 'X';
-    }
+fn traverse_map(
+    map: &HashMap<Coords, char>,
+    guard: &Guard,
+    obstacle: Option<&Coords>,
+) -> (Guard, bool) {
+    let mut guard = guard.clone();
     let mut infinite_loop = false;
     loop {
-        infinite_loop = guard.walk(&map);
+        infinite_loop = guard.walk(&map, obstacle);
         if infinite_loop {
             break;
         }
-        if let Some(next_pos) = map.get_mut(&guard.coords) {
-            if next_pos == &'.' {
-                *next_pos = 'X';
-            }
-        } else {
+        if Option::None == map.get(&guard.coords) {
             break;
         }
         // print_map(&map);
     }
-    (map, infinite_loop)
+    (guard, infinite_loop)
 }
 
 fn is_infinit_loop_when_adding_obstacle_at(
-    mut guard: Guard,
-    mut map: HashMap<Coords, char>,
-    obstacle: Coords,
+    guard: &Guard,
+    map: &HashMap<Coords, char>,
+    obstacle: &Coords,
 ) -> bool {
-    map.insert(obstacle, '#');
-    let guard = guard.clone();
-    traverse_map(map, guard).1
+    traverse_map(map, guard, Option::Some(obstacle)).1
 }
 
 fn print_map(map: &HashMap<Coords, char>) {
@@ -156,8 +159,8 @@ mod tests {
         let input = get_input(file!(), "example.txt");
         let map = parse_input(input);
         let guard = find_guard(&map);
-        let map = traverse_map(map, guard).0;
-        assert_eq!(41, map.values().filter(|&c| *c == 'X').count());
+        let final_guard = traverse_map(&map, &guard, Option::None).0;
+        assert_eq!(41, final_guard.previous_positions.len());
     }
 
     #[test]
@@ -165,8 +168,8 @@ mod tests {
         let input = get_input(file!(), "input.txt");
         let map = parse_input(input);
         let guard = find_guard(&map);
-        let map = traverse_map(map, guard).0;
-        assert_eq!(5129, map.values().filter(|&c| *c == 'X').count());
+        let final_guard = traverse_map(&map, &guard, Option::None).0;
+        assert_eq!(5129, final_guard.previous_positions.len());
     }
 
     #[test]
@@ -174,14 +177,13 @@ mod tests {
         let input = get_input(file!(), "example.txt");
         let map = parse_input(input);
         let guard = find_guard(&map);
-        let map_with_x = traverse_map(map.clone(), guard.clone());
+        let map_with_x = traverse_map(&map, &guard, Option::None);
         let result = map_with_x
             .0
-            .iter()
-            .filter(|(coords, c)| *c == &'X' || *coords == &guard.coords)
-            .map(|(coords, _)| *coords)
+            .previous_positions
+            .keys()
             .map(|obstacle| {
-                if is_infinit_loop_when_adding_obstacle_at(guard.clone(), map.clone(), obstacle) {
+                if is_infinit_loop_when_adding_obstacle_at(&guard, &map, obstacle) {
                     1
                 } else {
                     0
@@ -196,14 +198,12 @@ mod tests {
         let input = get_input(file!(), "input.txt");
         let map = parse_input(input);
         let guard = find_guard(&map);
-        let map_with_x = traverse_map(map.clone(), guard.clone());
-        let result = map_with_x
-            .0
+        let map_with_x = traverse_map(&map, &guard, Option::None);
+        let obstacles: Vec<&Coords> = map_with_x.0.previous_positions.keys().collect();
+        let result = obstacles
             .par_iter()
-            .filter(|(coords, c)| *c == &'X' || *coords == &guard.coords)
-            .map(|(coords, _)| *coords)
             .map(|obstacle| {
-                if is_infinit_loop_when_adding_obstacle_at(guard.clone(), map.clone(), obstacle) {
+                if is_infinit_loop_when_adding_obstacle_at(&guard, &map, *obstacle) {
                     1
                 } else {
                     0
---
+++ src/_2024/day7/solution.rs
@@ -0,0 +1,99 @@
+#[derive(Clone)]
+enum Operation {
+    Add,
+    Multiply,
+}
+
+fn parse_input(input: String) -> Vec<(u128, Vec<u128>)> {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(": ");
+            let result: u128 = parts.next().unwrap().parse().unwrap();
+            let mut parts = parts.next().unwrap().split(" ");
+            let numbers: Vec<u128> = parts.map(|part| part.parse().unwrap()).collect();
+            (result, numbers)
+        })
+        .collect()
+}
+
+fn can_operations_combine_result(input: &(u128, Vec<u128>)) -> bool {
+    let (result, numbers) = input;
+    if numbers.iter().any(|n| n >= &&result) {
+        return false;
+    }
+    if &numbers.iter().filter(|nr| nr != &&1u128).sum::<u128>() > result {
+        return false;
+    }
+    for operation_mix in 0..2usize.pow((numbers.len() - 1) as u32) {
+        // println!();
+        // println!();
+        let mut tmp_result = numbers[0];
+        // print!("{} ", tmp_result);
+        for number_index in 1..numbers.len() {
+            let operation = (operation_mix >> (number_index - 1)) & 1;
+            // print!("{} ", if operation == 0 { " + " } else { " * " });
+            // print!("{}", numbers[number_index]);
+
+            match operation {
+                0 => tmp_result += numbers[number_index],
+                1 => tmp_result *= numbers[number_index],
+                _ => unreachable!(),
+            }
+            if &tmp_result == result {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::shared::file_parser::get_input;
+
+    use super::*;
+
+    #[test]
+    fn example_1_can_operation_combine_result() {
+        assert_eq!(true, can_operations_combine_result(&(5, vec![1, 2, 3])));
+    }
+
+    #[test]
+    fn example_1() {
+        let input = get_input(file!(), "example.txt");
+        let parsed = parse_input(input);
+        assert_eq!(
+            3749u128,
+            parsed
+                .into_iter()
+                .filter(|i| can_operations_combine_result(i))
+                .map(|(result, _)| result)
+                .sum()
+        );
+    }
+
+    #[test]
+    fn input_1() {
+        let input = get_input(file!(), "input.txt");
+        let parsed = parse_input(input);
+        assert_eq!(
+            3749u128,
+            parsed
+                .into_iter()
+                .filter(|i| can_operations_combine_result(i))
+                .map(|(result, _)| result)
+                .sum()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        let input = get_input(file!(), "example.txt");
+    }
+
+    #[test]
+    fn input_2() {
+        let input = get_input(file!(), "input.txt");
+    }
+}
